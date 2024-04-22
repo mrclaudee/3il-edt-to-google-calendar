00a8fa7e266fb1b913d3ccd7b6a3b11d8c6ff820
Claude FOTSO <fotso1claude@gmail.com>
2024-04-22 16:07:58 +0200

    Update logout

--- src/sharelockr/urls.py
+++ src/sharelockr/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index
+from .views import index, logout_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('', include('edt.urls')),
     path('', include('allauth.urls')),
+    path('logout/', logout_view, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- src/sharelockr/views.py
+++ src/sharelockr/views.py
@@ -1,7 +1,7 @@
 import os
-from datetime import datetime
 
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,3 +10,7 @@ load_dotenv()
 def index(request):
     return render(request, "sharelockr/index.html", context={'google_id': os.getenv('GOOGLE_ID_CLIENT')})
 
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
